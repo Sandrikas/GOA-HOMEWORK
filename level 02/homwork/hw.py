3d56fd729552a606f84029edefa25f4d65622d5a
Sandrikas <Sgogelashvili72@gmail.com>
2025-02-22 20:37:35 +0400

    commit

--- level 02/homwork/hw.py
+++ level 02/homwork/hw.py
@@ -1,1 +1,23 @@
-# 
+# # 1. სტრინგი 'გამარჯობას'
+greetings = "გამარჯობას"
+print(greetings)
+
+# 2. სტრინjი 'ელემენტი', 10-ჯერ ერთ ხაზზე
+element = "ელემენტი"
+print((element + " ") * 10)
+
+# 3. ინტეჯერი 1000- ორი  რიცვის მეშვეობით
+num1 = 500
+num2 = 500
+integer_sum = num1 + num2
+print(integer_sum)
+
+# 4. სტრინგი 'hello world', ორი სტრინგის მეშვეობით
+part1 = "hello"
+part2 = " world"
+hello_world = part1 + part2
+print(hello_world)
+
+# 5. ბულეანური მნიშვნელობა, არა პირდაპირ True ან False-ის დაწერით
+boolean_value = (5 * 2) == 10  # 5 * 2 = 10, შესაბამისად True
+print(boolean_value)
