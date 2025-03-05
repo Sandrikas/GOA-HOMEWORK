08cb7dbc9cb571cee3e55a627983c331e103202b
Sandrikas <Sgogelashvili72@gmail.com>
2025-03-05 18:39:31 +0400

    commit

---
+++ level03/classwork/cw.py
@@ -0,0 +1,39 @@
+# მომხმარებელს შემოატანინეთ სამი რიცხვი და გამოიტანეთ ამ რიცხვების ჯამი
+# age1=int(input("shemoitane sheni asaki:"))
+# age2=int(input("shemoitane meore asaki:"))
+# age3=int(input("shemoitane mesame asaki:"))
+# print(age1+age2+age3)
+
+
+
+#  შემოატანინეთ მომხმარებელს სახელი და შეინახეთ მისი სახელი ცვლადში
+# name=input("shemoitane saxeli")
+
+
+
+
+# შემოიტანინეთ მომხმარელს ორი რიცხვი, თუ პირველი რიცხვი მეტია მეორეზე გამოიტანეთ True ( boolean ) თუ არადა გამოიტანეთ False ( boolean )
+# number1=int(input("shemoitane pirveli ricxvi:"))
+# number2=int(input("shemoitane meore ricxvi:"))
+# if number1>number2:
+#     print(True)
+# else:
+#     print(False)
+
+
+
+# მომხმარებელს შემოვატანინოთ სიტყვა და n რიცხვი, შემდეგ გამოვიტანოთ ეს სიტყვა n-ჯერ.
+# number=int(input("shemoitane ricxvi:"))
+# word=input('shemoitane sityva:')
+# print(word*number)
+
+
+
+# 5) შემოაყვანინეთ მომხმარებელს ორი რიცხვი, და გამოპრინტეთ ამ ორი რიცხვის გაერთიანებულ ვერსისას დამატებული 1, მაგ თუ ჯერ შემოვიდა ათი შემდეგ 11, თქვენი მიზანია გამოიტანოთ 1012, ვინაიდან 10 გაერთანებული 11 არის 1011 და 1011 + 1 არის 1012
+# number1=input("shemoiyvane pirveli ricxvi;")
+# number2=input("shemoiyvane meore ricxvi:")
+# final=number1+number2
+# print(int(final)+1)
+
+
+
