c451cfda89daa5bb573b6fc4337bcb96df3ec7ad
Sandrikas <Sgogelashvili72@gmail.com>
2025-03-08 18:33:35 +0400

    commit

---
+++ level05/homework/hw.py
@@ -0,0 +1,27 @@
+# for ციკლის გამოყენებით რიცხვების 1-დან 10-მდე გამოტანა
+# for i in range(1, 11):
+#     print(i)
+
+
+# for ციკლის გამოყენებით რიცხვების 5-დან 25-მდე გამოტანა
+# for i in range(5, 26):
+#     print(i)
+
+
+# for ციკლის გამოყენებით რიცხვების 10-დან 100-მდე, 5-ის step-ით გამოტანა
+# for i in range(10, 101, 5):
+#     print(i)
+
+
+
+
+
+# word = input("შეიყვანეთ სიტყვა: ")
+# result = ''.join([char for char in word if char == 'A'])
+# print(result)
+
+
+
+
+
+
